35b3ae976d00dd19fd49bb0219de6f4eeb22b287
AUREOLE\vietnh <vietnh1508@gmail.com>
2022-12-28 08:09:28 +0700

    10

--- pages/index.js
+++ pages/index.js
@@ -318,7 +318,7 @@ export default function Index() {
           //  ref={qrRef}
           delay={300}
           style={{ width: '180px' }}
-          constraints={{ audioL: false; video: { facingMode: "environmemnt" } }}
+          constraints={{ audio: false, video: { facingMode: "environmemnt" } }}
           onError={handleErrorWebCam}
           onScan={handleScanWebCam}
         />}
